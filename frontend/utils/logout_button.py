f2b47928acc286391bad53c85ceade3ad88c171f
Bhargav Vivek Modak <bhargav0modak@gmail.com>
2024-09-14 15:57:51 +0530

    feat: add persisted login state with cookies
    - add new library `streamlit-cookies-manager`
    - fix cache deprecation warning by updating above library code
    - save `g_session` to cookies
    - remove `g_session` on logout

--- frontend/utils/logout_button.py
+++ frontend/utils/logout_button.py
@@ -1,8 +1,16 @@
 import streamlit as st
+import time
+from frontend.utils.browser import reload_page
 
 
-def logout_button():
+def logout_button(cookies):
+    if not cookies.ready():
+        st.stop()
+
     if st.button("Logout"):
         st.info("Logging out...")
         st.session_state.clear()
-        st.rerun()
+        cookies.set("g_session", "")
+        cookies.save()
+        time.sleep(1)
+        reload_page()
--- frontend/utils/sidebar.py
+++ frontend/utils/sidebar.py
@@ -2,7 +2,7 @@ import streamlit as st
 from frontend.utils.logout_button import logout_button
 
 
-def set_sidebar(user: dict):
+def set_sidebar(user: dict, cookies):
 
     st.sidebar.title("Which-Group-What-Project")
     st.sidebar.write(f"Welcome, {user['full_name']}!")
@@ -12,4 +12,4 @@ def set_sidebar(user: dict):
     st.sidebar.page_link("pages/add_team.py", label="➕ Add Team")
     st.sidebar.page_link("pages/update_team.py", label="🔄 Update Team")
     with st.sidebar:
-        logout_button()
+        logout_button(cookies)
